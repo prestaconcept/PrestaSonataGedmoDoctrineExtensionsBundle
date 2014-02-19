a9fc41579d944a80760d829f4a1c137a9de98d2e
Nicolas Bastien <nbastien@prestaconcept.net>
2014-02-19 12:43:24 +0100

    prestaconcept/open-source-management#15 :implement sonata block event

---
+++ Block/LocaleSwitcherBlockService.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file is part of the PrestaSonataGedmoDoctrineExtensionsBundle.
+ *
+ * (c) PrestaConcept <http://www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\SonataGedmoDoctrineExtensionsBundle\Block;
+
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\BlockBundle\Block\BaseBlockService;
+use Sonata\BlockBundle\Model\BlockInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ */
+class LocaleSwitcherBlockService extends BaseBlockService
+{
+    /**
+     * {@inheritdoc}
+     */
+    function setDefaultSettings(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'admin'     => null,
+                'object'    => null,
+                'template'  => 'PrestaSonataGedmoDoctrineExtensionsBundle:Block:block_locale_switcher.html.twig',
+            )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateBlock(ErrorElement $errorElement, BlockInterface $block)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildEditForm(FormMapper $formMapper, BlockInterface $block)
+    {
+    }
+}
